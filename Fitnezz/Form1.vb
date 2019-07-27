caac5c03fe2c3e648487741a82a6e2359b114c08
Nazibul Alam <nalam8693@gmail.com>
2019-07-27 03:53:05 -0700

    chng

--- Fitnezz/Form1.vb
+++ Fitnezz/Form1.vb
@@ -5,8 +5,4 @@ Public Class Form1
         FitnezzMainScreen.Show()
         Close()
     End Sub
-
-	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-	End Sub
 End Class
